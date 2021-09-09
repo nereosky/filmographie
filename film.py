b28f745178310fec57bf6b55ace4dc439d349a5d
kilian longepe <kilian.longepe@gmail.com>
2021-09-09 09:26:49 +0200

    add template

--- film.py
+++ film.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template, jsonify, make_response,abort
+
 import json
 import os, sys
+
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT",5000))
@@ -19,12 +21,12 @@ def get_film_title(title):
 	film = [film for film in films if film["Title"] == title ]
 	if len(film) ==0:
 		abort(404,"film with title::{} does not exit".format(title))
-	return jsonify({"films":film})
+	return render_template('film.html', title="film", jsonfile=json.dumps({"films":film}))
 
 @app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify({'error':'Not Found'}),404)
 
 if __name__ == "__main__":        
-    app.run(debug=True,host='127.0.0.1',port=port) 
-	# app.run(host = "0.0.0.0", port = port, debug=True)                
+    # app.run(debug=True,host='127.0.0.1',port = port) 
+	app.run(host = "0.0.0.0", port = port, debug=True)                
