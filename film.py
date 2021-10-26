037e76f0b606b655e2f8af7a54790ca40a7f6245
kilian longepe <kilian.longepe@gmail.com>
2021-10-26 15:23:06 +0200

    add template

--- film.py
+++ film.py
@@ -1,8 +1,9 @@
 from flask import Flask,render_template, jsonify, request , make_response
 from firebase_admin import credentials, firestore, initialize_app
-
+from jinja2 import Template
 
 import os
+
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT",5000))
@@ -15,58 +16,64 @@ doc_ref = db.collection('bddfilms').document('ZaE2fwAxJfzrq0PdWFPw')
 emp_ref = db.collection('bddfilms')
 docs =emp_ref.stream()
 
+indexLink = "Accueil"
+
 @app.route('/')
 def index():
-	return '/list to see all films, /add to add new film, /update to update a film and /delete to delete a film'
+    listLink = "list"
+    addLink = "add"
+    updateLink= "update"
+    deleteLink = "delete"    
+    return render_template("index.html", list_link = listLink, add_link = addLink, update_link = updateLink, delete_link = deleteLink)
+
+@app.route('/list', methods=['GET'])
+def read():
+    
+    """
+        read() : Fetches documents from Firestore collection as JSON
+        all_todos : Return all documents
+    """
+
+    data = [doc.to_dict() for doc in docs]
+    
+    return render_template("list.html", index_link = indexLink, bdd_data = data)
 
 @app.route('/add', methods=['POST'])
 def create():
+    return render_template("add.html", index_link = indexLink)
     """
         create() : Add document to Firestore collection 
        
-    """
+    
     try:
         id = request.json['id']
         doc_ref.document(id).set(request.json)
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
-
-
-@app.route('/list', methods=['GET'])
-def read():
     """
-        read() : Fetches documents from Firestore collection as JSON
-        all_todos : Return all documents
-    """
-    try:
-
-        docs =emp_ref.stream()
-        all_todos = [doc.to_dict() for doc in docs]
-        return jsonify(all_todos), 200
-
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
 
 @app.route('/update', methods=['POST', 'PUT'])
 def update():
+    return render_template("update.html", index_link = indexLink)
     """
         update() : Update document in Firestore collection
-    """
+    
     try:
         id = request.json['id']
         doc_ref.document(id).update(request.json)
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+    """
 
 
 @app.route('/delete', methods=['GET', 'DELETE'])
 def delete():
+    return render_template("delete.html", index_link = indexLink)
     """
         delete() : Delete a document from Firestore collection
-    """
+    
     try:
         # Check for ID in URL query
         todo_id = request.args.get('id')
@@ -74,6 +81,7 @@ def delete():
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+    """
 
 @app.errorhandler(404)
 def not_found(error):
